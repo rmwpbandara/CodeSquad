f2407af5ba9788a6ea2869f842d4443b1d24928a
Wasantha P Bandara <rmwpbandara@gmail.com>
2022-04-23 07:26:50 -0700

    update new file

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         file = sys.argv[1]
     else:
-        file = "home_1.jpg"
+        file = "Login_3.jpg"
     print('[INFO] start processing file: ' + file)
 
     # execute main process
