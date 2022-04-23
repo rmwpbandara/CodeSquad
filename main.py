3c015fb762bfddd3ab14646660ac61ea34d449f3
Wasantha <wasantha@fh.technology>
2022-04-23 12:23:40 +0530

    test

--- main.py
+++ main.py
@@ -37,8 +37,6 @@ def run(file):
             w = w + 4
             h = h + 4
             crop_img = img[y:y + h, x:x + w]  # crop image using bounding rectangle
-            # cv2.imshow('crop', crop_img)
-            # cv2.waitKey(0)
 
             width_crop, height_crop, channels_crop = crop_img.shape  # find width and height copped image
 
